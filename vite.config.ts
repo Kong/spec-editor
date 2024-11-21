aa9a3a7487dbf8bf44072a6ce2d7c96065a10406
Val Gorodnichev <val.gorodnichev@konghq.com>
2024-11-21 11:23:30 -0700

    fix: externalise shiki [KHCP-14202]

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+// !Important: always externalize `shiki/onig.wasm`
+const externalDependencies: string[] = ['shiki/onig.wasm']
+
 // https://vite.dev/config/
 export default defineConfig({
   base: '/spec-editor',
@@ -14,6 +17,11 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  build: {
+    rollupOptions: {
+      external: externalDependencies,
     },
   },
   css: {
